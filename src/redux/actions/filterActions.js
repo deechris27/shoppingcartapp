4ecb81aad1ab838977233a7592862f8acdc90542
Deepak <dd43028@gmail.com>
2020-05-07 12:33:50 +0530

    range filter fix

--- src/redux/actions/filterActions.js
+++ src/redux/actions/filterActions.js
@@ -1,8 +1,14 @@
 export const applyRangeFilter = (state, minmax) => {
     const [min, max] = minmax;
+    let newItems = JSON.parse(localStorage.getItem("mainState"));
+    let result;
     if(min===0 && max==="100000"){
-        return JSON.parse(localStorage.getItem("mainState"));
-    }else{
-        return state.items.filter(item => item.price.actual >= min && item.price.actual <= max);
+        result = JSON.parse(localStorage.getItem("mainState"));
     }
+    if(!state.items.length || state.items.length < 8){
+        result = newItems.filter(item => item.price.actual >= min && item.price.actual <= max);
+    }else{
+        result = state.items.filter(item => item.price.actual >= min && item.price.actual <= max);
+    }
+    return result;
 };
